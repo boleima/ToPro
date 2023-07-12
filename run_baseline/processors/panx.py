a29e54b4fd7eaad2f42294d473d0553f35a97da1
boleima <107153303+boleima@users.noreply.github.com>
2023-07-13 01:36:32 +0200

    Update panx.py

--- run_baseline/processors/panx.py
+++ run_baseline/processors/panx.py
@@ -32,11 +32,11 @@ class PanxProcessor(DataProcessor):
     def __init__(self):
         pass
 
-    def get_examples(self, data_dir, language='en', split='train', num_sample=-1, model="bert-base-multilingual-cased"):
+    def get_examples(self, data_dir, model, language='en', split='train', num_sample=-1):
         """See base class."""
         examples = []
         for lg in language.split(','):
-            data_file = os.path.join(data_dir, lg, "{}.{}".format("dev", model))
+            data_file = os.path.join(data_dir, lg, "{}.{}".format(split, model))
             with open(data_file, encoding="utf-8") as f:
                 words=[]
                 labels=[]
@@ -90,14 +90,14 @@ class PanxProcessor(DataProcessor):
 
         return new_example
 
-    def get_train_examples(self, data_dir, language='en', num_sample=-1, model="bert-base-multilingual-cased"):
-        return self.get_examples(data_dir, language, split='train', num_sample=num_sample, model=model)
+    def get_train_examples(self, data_dir, model, language='en', num_sample=-1):
+        return self.get_examples(data_dir, model, language, split='train', num_sample=num_sample)
 
-    def get_dev_examples(self, data_dir, language='en', num_sample=-1, model="bert-base-multilingual-cased"):
-        return self.get_examples(data_dir, language, split='dev', num_sample=num_sample, model=model)
+    def get_dev_examples(self, data_dir, model, language='en', num_sample=-1):
+        return self.get_examples(data_dir, model, language, split='dev', num_sample=num_sample)
 
-    def get_test_examples(self, data_dir, language='en', num_sample=-1, model="bert-base-multilingual-cased"):
-        return self.get_examples(data_dir, language, split='test', num_sample=num_sample, model=model)
+    def get_test_examples(self, data_dir, model, language='en', num_sample=-1):
+        return self.get_examples(data_dir, model, language, split='test', num_sample=num_sample)
 
     def get_translate_train_examples(self, data_dir, language='en', num_sample=-1):
         """See base class."""
