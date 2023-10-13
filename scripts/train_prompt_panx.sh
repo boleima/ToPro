c7044e1c563c36978512b6a80794b7151ed1378b
Bolei Ma <107153303+boleima@users.noreply.github.com>
2023-10-13 11:39:12 +0200

    Update train_prompt_panx.sh

--- scripts/train_prompt_panx.sh
+++ scripts/train_prompt_panx.sh
@@ -25,8 +25,7 @@ elif [ $MODEL == "xlm-mlm-100-1280" ] || [ $MODEL == "xlm-mlm-tlm-xnli15-1024" ]
 elif [ $MODEL == "xlm-roberta-large" ] || [ $MODEL == "xlm-roberta-base" ]; then
   MODEL_TYPE="xlmr"
 fi
-#SEEDS=(10 42 421 520 1218)
-SEEDS=(42 421 520 1218)
+SEEDS=(10 42 421 520 1218)
 
 if [ $MODEL == "xlm-mlm-100-1280" ] || [ $MODEL == "xlm-roberta-large" ]; then
   BATCH_SIZE=2
@@ -38,7 +37,7 @@ else
   #LR=1e-5
 fi
 
-runfewshot(){
+run(){
   NAME="${MODEL}-LR${LR}-epoch${EPOCH}-MaxLen${MAXL}-Pattern${PATTERN_ID}-seed${1}"
   SAVE_DIR="$OUT_DIR/$TASK/$PATTERN_ID/${NAME}/"
   RESULT_FILE="results_${TASK}_full.csv"
@@ -64,9 +63,7 @@ runfewshot(){
     --eval_test_set $LC \
     --pattern_id $PATTERN_ID \
     --seed ${1} \
-    --early_stopping
-  #  --init_checkpoint outputs/xnli/bert-base-multilingual-cased-LR2e-5-epoch5-MaxLen128-PatternID1/checkpoint-best/
-	
+    --early_stopping	
   python $PWD/results_to_csv.py \
     --input_path "${SAVE_DIR}test_results.txt" \
     --save_path $RESULT_FILE \
@@ -75,8 +72,5 @@ runfewshot(){
 
 for SEED in "${SEEDS[@]}"
 do
-  runfewshot $SEED
+  run $SEED
 done
-
-#--overwrite_cache \
-
