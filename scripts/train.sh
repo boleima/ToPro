8f14588865aa4c45fd8a62245662f1a226dec3e1
Bolei Ma <boleimabl@gmail.com>
2023-10-13 18:53:47 +0200

    +

--- scripts/train.sh
+++ scripts/train.sh
@@ -9,16 +9,16 @@ if [$MODEL==mt5-base]
     then
         if [$METHOD==topro]
             then
-                sh scripts/train_prompt_{$TASK}_t5.sh 0 google/mt5-base
+                source scripts/train_prompt_{$TASK}_t5.sh 0 google/mt5-base
             else
-                sh scripts/train_{$TASK}_t5.sh google/mt5-base 0
+                source scripts/train_{$TASK}_t5.sh google/mt5-base 0
         fi
     else
         if [$METHOD==topro]
             then
-                sh scripts/train_prompt_{$TASK}.sh 0 $MODEL
+                source scripts/train_prompt_{$TASK}.sh 0 $MODEL
             else
-                sh scripts/train_{$TASK}.sh $MODEL 0
+                source scripts/train_{$TASK}.sh $MODEL 0
         fi
 fi
 
